b2855d67573997053b24e8c522bde91df3eabde8
James Smith <james@floppy.org.uk>
2014-05-08 13:38:14 +0100

    Add new foreman suite

---
+++ test/integration/simple-deployment-new-foreman/serverspec/localhost/simple_deployment_spec.rb
@@ -0,0 +1,44 @@
+require 'spec_helper'
+
+# Make sure nginx is running
+describe service("nginx") do
+  it { should be_running }
+end
+
+# Make sure vhosts have the correct stuff
+describe file("/etc/nginx/sites-enabled/deploytest.theodi.org") do
+  it { should be_file }
+  its(:content) { should match "server_name deploytest.theodi.org;" }
+  its(:content) { should match "proxy_pass http://deployment-test-app;" }
+end
+
+# Make sure we have some code
+describe file("/var/www/deploytest.theodi.org/current/hello.rb") do
+  it { should be_file }
+end
+
+# Make sure we have environment correctly
+describe file("/var/www/deploytest.theodi.org/current/.env") do
+  its(:content) { should match /SUCH: test/ }
+end
+
+describe file("/etc/init/deployment-test-app-web-1.conf") do
+  its(:content) { should match /SUCH=test/ }
+  its(:content) { should match /PORT=3000/ }
+  its(:content) { should match /bundle exec ruby hello.rb -p \$PORT/ }
+end
+
+# Make sure foreman job is running
+describe service("deployment-test-app-web-1") do
+  it { should be_running }
+end
+
+# Check we can actually access the thing
+describe command("curl -H 'Host: deploytest.theodi.org' localhost") do
+  it { should return_stdout /Hello, world!/ }
+end
+
+# Check we can actually access the thing
+describe command("curl -H 'Host: deploytest.theodi.org' localhost") do
+  it { should return_stdout /Hello, world!/ }
+end
