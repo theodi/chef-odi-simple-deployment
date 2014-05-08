fe1b553b94965a5303b4115e8e3be33bb7ab2c1b
James Smith <james@floppy.org.uk>
2014-05-08 12:17:53 +0100

    Add deployment and tests for old style foreman jobs

---
+++ test/integration/simple-deployment-old-foreman/serverspec/localhost/simple-deployment.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+# Make sure nginx is running
+describe service("nginx") do
+  it { should be_running }
+end
+
+# Make sure vhosts have the correct stuff
+describe file("/etc/nginx/sites-enabled/deploytest.theodi.org") do
+  it { should be_file }
+  its(:content) { should match "server_name deploytest.theodi.org;" }
+  its(:content) { should match "proxy_pass http://deployment-test-app;" }
+end
+
+# Make sure we have some code
+describe file("/var/www/deploytest.theodi.org/current/app.rb") do
+  it { should be_file }
+end
+
+# Make sure we have environment correctly
+describe file("/var/www/deploytest.theodi.org/current/.env") do
+  its(:content) { should match /SUCH: test/ }
+end
+
+# Make sure foreman job is running
+describe service("deployment-test-app-web-1") do
+  it { should be_running }
+end
+
+# Check we can actually access the thing
+describe command("curl -H 'Host: deploytest.theodi.org' localhost") do
+  it { should return_stdout /Hello, world!/ }
+end
